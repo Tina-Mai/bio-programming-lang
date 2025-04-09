f456fd6f7a9536404ea72a3dce3c6b27ecc28ab2
Tina Mai <tinabmai@gmail.com>
2025-04-09 03:48:46 -0700

    configurable constraints

--- src/components/canvas/blocks/Container.tsx
+++ src/components/canvas/blocks/Container.tsx
@@ -1,6 +1,9 @@
 import React, { MouseEvent } from "react";
 import { ContainerData } from "@/types";
 import { NodeProps } from "@xyflow/react";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Add } from "@carbon/icons-react";
 
 export const ContainerNode = ({ data, selected }: NodeProps) => {
 	const containerData = data as ContainerData;
@@ -29,7 +32,17 @@ export const ContainerNode = ({ data, selected }: NodeProps) => {
 			onMouseDown={onMouseDown}
 			onClick={(event) => event.stopPropagation()}
 		>
-			{containerData.label && <div className="font-mono text-sm text-zinc-600 py-3 px-4">{containerData.label}</div>}
+			<div className="horizontal my-3 mx-4 items-center gap-1 flex-wrap">
+				{containerData.label &&
+					containerData.label.split(", ").map((label, index) => (
+						<Badge key={index} variant="outline" className="text-zinc-600 capitalize">
+							{label}
+						</Badge>
+					))}
+				<Button size="icon" variant="outline" className="!px-0 !py-0 !size-5 !rounded-xs" onClick={(e) => e.stopPropagation()}>
+					<Add size={4} />
+				</Button>
+			</div>
 		</div>
 	);
 };

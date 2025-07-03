2b4daeadcd3e55ecfcad89915ba0321f44200e69
Tina Mai <tinabmai@gmail.com>
2025-07-03 10:27:29 -0700

    colors

--- src/types/Segment.ts
+++ src/types/Segment.ts
@@ -12,13 +12,9 @@ export type SegmentType = "CDS" | "promoter" | "terminator";
 // TODO: color for each segment type
 export const SEGMENT_COLORS: Record<SegmentType | "default", { fill: string; stroke: string; highlight: string }> = {
 	default: {
-		// fill: "rgb(165 180 252 / 0.45)", // indigo
-		// stroke: "rgb(129 140 248 / 0.8)",
-		// fill: "#ECEEF6", // bluish gray
-		// stroke: "#8EAEEB",
-		fill: "#ECF1FE",
-		stroke: "#AAC1FF",
-		highlight: "#9CB4F4",
+		fill: "oklch(0.9582 0.0183 269.09)",
+		stroke: "oklch(0.766 0.093 268.47 / 0.7)",
+		highlight: "oklch(0.766 0.093 268.47)",
 	},
 	CDS: {
 		fill: "rgb(253 224 71 / 0.45)", // yellow
